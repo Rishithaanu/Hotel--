c3ae2dc0b72774a0f4dbbab74975c62ec245be84
Rishithaanu <162281816+Rishithaanu@users.noreply.github.com>
2024-03-13 14:11:49 +0530

    Add files via upload

---
+++ BeafSteak.js
@@ -0,0 +1,32 @@
+var num =0;
+
+function inc(){
+    num++;
+    if(num>=50)
+    {
+        num=50;
+        document.getElementById("inpt").value=num;
+    }
+    else{
+        document.getElementById("inpt").value=num;
+    }
+}
+function dec(){
+    num--;
+    if(num<=0)
+    {
+        num=0;
+        document.getElementById("inpt").value=num; 
+    }
+    else{
+        document.getElementById("inpt").value=num;
+    }
+}
+var price = 60;
+function total(){
+    var totalPrice = price*num;
+    document.getElementById("total").innerHTML=`Total Order Price :`+totalPrice+`$/-`;
+}
+function submit(){
+    document.getElementById("submit").innerHTML=`Your Order Confirmed!`
+}
