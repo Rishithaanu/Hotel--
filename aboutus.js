b3be209c9f0860b767f8a12fff22f84888e0020a
Rishithaanu <162281816+Rishithaanu@users.noreply.github.com>
2024-03-13 14:33:04 +0530

    Add files via upload

---
+++ aboutus.js
@@ -0,0 +1,4 @@
+function rating(){
+    // document.getElementsByTagName("input")[0].innerHTML="Thank You for Your Response!";
+    document.getElementById("ip").innerHTML=`Thankyou for your response!`
+}
